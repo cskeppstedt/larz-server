fbc0ad83fd7080680ec892512fae689e0c00e421
Christoffer Skeppstedt <c.skeppstedt@gmail.com>
2015-02-07 19:06:14 +0000

    Add a basic way to configure the app

---
+++ vars.py
@@ -0,0 +1,7 @@
+import os
+
+EMAIL        = os.environ['LARZSERVER_EMAIL']
+SECRET       = os.environ['LARZSERVER_FIREBASE_SECRET']
+FB_URL       = os.environ['LARZSERVER_FIREBASE_URL']
+API_BASE_URL = os.environ['LARZSERVER_API_URL']
+API_TOKEN    = os.environ['LARZSERVER_API_TOKEN']
