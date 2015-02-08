fb46b4904f52f18b895bc305241d2fc05a1ccc45
Christoffer Skeppstedt <c.skeppstedt@gmail.com>
2015-02-08 16:37:50 +0000

    Finally got mocked HTTP requests for testing

--- poll.py
+++ poll.py
@@ -1,3 +1,13 @@
+import requests
+import vars
+
+
 class Poll:
     def matches(self, list_of_userid):
-        pass
+        last_response = None
+
+        for userid in list_of_userid:
+            url = "{}/match_history/ranked/nickname/{}/?token={}".format(vars.API_BASE_URL, userid, vars.API_TOKEN)
+            last_response = requests.get(url)
+        
+        return last_response.json()
--- test_poll.py
+++ test_poll.py
@@ -1,5 +1,10 @@
 from pytest_bdd import scenario, given, when, then
 from poll import Poll
+import httpretty
+import re
+import requests
+import vars
+
 
 @scenario('poll.feature', 'Polling matches')
 def test_polling_matches():
@@ -7,11 +12,33 @@ def test_polling_matches():
 
 @given('a list of userid')
 def list_of_userid():
-    return ['1','2','3']
+    return ['1','2']
 
 @then('it should pull matches for each userid')
 def pull_foreach_userid(list_of_userid):
-    pass
+    body = '{"asd": "foo"}'
+    req_urls = []
+
+    def make_url(userid): return "{}/match_history/ranked/nickname/{}/?token={}".format(vars.API_BASE_URL, userid, vars.API_TOKEN)
+    expected_urls = map(make_url, list_of_userid)
+    
+    def cb(req, uri, headers):
+        req_urls.append(uri)
+        return (200, headers, body)
+    
+    httpretty.enable()
+    httpretty.register_uri(httpretty.GET, re.compile("(.*)"),
+                           body=cb,
+                           content_type="application/json")
+
+    instance = Poll()
+    result = instance.matches(list_of_userid)
+
+    assert result == {'asd': 'foo'}
+    assert req_urls == expected_urls
+
+    httpretty.disable()
+    httpretty.reset()
 
 @then('it should pull stats for the 10 latest, unique matches')
 def pull_matches(list_of_userid):
