aca76f627052b0501f5e96fd80cd50b1110cd681
Christoffer Skeppstedt <c.skeppstedt@gmail.com>
2015-02-10 23:00:19 +0100

    Implement match_history polling

--- poll.py
+++ poll.py
@@ -1,13 +1,25 @@
 import requests
 import vars
+import helpers
+import re
 
 
 class Poll:
     def matches(self, list_of_userid):
         last_response = None
+        id_set = set([])
+        expr = re.compile("([0-9]+)\|[^,]*")
 
         for userid in list_of_userid:
-            url = "{}/match_history/ranked/nickname/{}/?token={}".format(vars.API_BASE_URL, userid, vars.API_TOKEN)
-            last_response = requests.get(url)
+            url = helpers.match_history_uri(userid)
+            response = requests.get(url).json()
+            for match_id in expr.findall(response[0]["history"]):
+                id_set.add(match_id)
+            
+        match_ids = list(id_set)
+        match_ids.sort()
+
+        match_ids_slug = "+".join(match_ids)
+        url = helpers.multi_match_uri(match_ids_slug)
+        response = requests.get(url).json()
         
-        return last_response.json()
--- test_poll.py
+++ test_poll.py
@@ -4,47 +4,71 @@ import httpretty
 import re
 import requests
 import vars
+import helpers
 
 
 @scenario('poll.feature', 'Polling matches')
 def test_polling_matches():
     pass
 
+
 @given('a list of userid')
 def list_of_userid():
     return ['1','2']
 
+
 @then('it should pull matches for each userid')
 def pull_foreach_userid(list_of_userid):
-    body = '{"asd": "foo"}'
     req_urls = []
+    expected_urls = map(helpers.match_history_uri, list_of_userid)
 
-    def make_url(userid): return "{}/match_history/ranked/nickname/{}/?token={}".format(vars.API_BASE_URL, userid, vars.API_TOKEN)
-    expected_urls = map(make_url, list_of_userid)
-    
-    def cb(req, uri, headers):
-        req_urls.append(uri)
-        return (200, headers, body)
-    
     httpretty.enable()
-    httpretty.register_uri(httpretty.GET, re.compile("(.*)"),
-                           body=cb,
-                           content_type="application/json")
+    add_fixtures(list_of_userid, helpers.match_history_uri, lambda _, uri, __: req_urls.append(uri))
 
     instance = Poll()
     result = instance.matches(list_of_userid)
 
-    assert result == {'asd': 'foo'}
-    assert req_urls == expected_urls
+    assert req_urls[:-1] == expected_urls
+    teardown()
 
-    httpretty.disable()
-    httpretty.reset()
 
 @then('it should pull stats for the 10 latest, unique matches')
 def pull_matches(list_of_userid):
-    pass
+    match_ids_slug = "136020720+136021420+136023435+136026090+136028023+136095625+136097421+136198877+136199362+136199918+136200860"
+    req_urls = []
+    expected_url = helpers.multi_match_uri(match_ids_slug)
+
+    httpretty.enable()
+    add_fixtures(list_of_userid, helpers.match_history_uri, lambda _, uri, __: req_urls.append(uri))
+    add_fixtures([match_ids_slug], helpers.multi_match_uri, lambda _, uri, __: req_urls.append(uri)) 
+
+    instance = Poll()
+    result = instance.matches(list_of_userid)
+
+    assert req_urls[-1] == expected_url
+    teardown()
+
 
 @then('it should return the matches')
 def no_duplicate_matches(list_of_userid):
     pass
 
+
+# --- helpers ---
+
+def add_fixtures(list_of_id, uri_fn, response_cb=None):
+    expected_urls = map(uri_fn, list_of_id)
+    
+    def cb(req, uri, headers):
+        if response_cb != None: response_cb(req, uri, headers)
+        body = helpers.fixture_for(uri)
+        return (200, headers, body)
+    
+    httpretty.register_uri(httpretty.GET,
+                           re.compile("(.*)"),
+                           body=cb,
+                           content_type="application/json")
+
+def teardown():
+    httpretty.disable()
+    httpretty.reset()
