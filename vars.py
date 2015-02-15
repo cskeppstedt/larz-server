a1960be7b623c533275dcdba1a6c68a8a26edc45
Christoffer Skeppstedt <c.skeppstedt@gmail.com>
2015-02-16 00:16:04 +0100

    Add USERS config

--- vars.py
+++ vars.py
@@ -6,3 +6,10 @@ SECRET       = os.getenv('LARZSERVER_FIREBASE_SECRET', default_value)
 FB_URL       = os.getenv('LARZSERVER_FIREBASE_URL', 'http://{}'.format(default_value))
 API_BASE_URL = os.getenv('LARZSERVER_API_URL', 'http://{}'.format(default_value))
 API_TOKEN    = os.getenv('LARZSERVER_API_TOKEN', default_value)
+USERS        = [
+    'skepparn_',
+    'Pacoloco',
+    'Adelsmansman',
+    'Schln',
+    'zwex'
+]
