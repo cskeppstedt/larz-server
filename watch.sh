778b367696e23a1c68bb4f504d410536c74259c3
Christoffer Skeppstedt <chris.skeppstedt@klarna.com>
2015-06-01 18:24:02 -0400

    Fix file watcher for OSX (requires fswatch)

--- watch.sh
+++ watch.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 DIRECTORY_TO_OBSERVE="."
+ON_CHANGE="py.test --feature features"
 
 function block_for_change {
   inotifywait -r \
@@ -7,12 +8,23 @@ function block_for_change {
     $DIRECTORY_TO_OBSERVE
 }
 
+function command_exists {
+  type "$1" &> /dev/null ;
+}
+
 function build {
-  py.test --feature features
+  eval $ON_CHANGE
+}
+
+function start_watch {
+  if command_exists fswatch; then
+    fswatch -o $DIRECTORY_TO_OBSERVE | xargs -n1 -I{} $ON_CHANGE
+  else
+    while block_for_change; do
+      build
+    done
+  fi
 }
 
 build
-
-while block_for_change; do
-  build
-done
+start_watch
